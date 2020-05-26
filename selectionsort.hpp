8f6bc533130a787044a7c1f9d380968aa1cab20a
s9rA16Bf4 <nicholas.rosqvist.nunes@gmail.com>
2020-05-27 00:11:16 +0200

    Add files via upload

---
+++ selectionsort.hpp
@@ -0,0 +1,44 @@
+#ifndef SELECTIONSORT_HPP
+#define SELECTIONSORT_HPP
+
+#include <vector>
+
+template<typename T>
+class selectionSort{
+private:
+  std::vector<T> internalVector; // This will hold our unsorted and hopefully sorted list
+
+public:
+  std::vector<T> getVector(); // Returns internalVector
+  void sort(); // Sorts our vector
+  void loadVector(std::vector<T> &orginVector);
+};
+
+template<typename T>
+std::vector<T> selectionSort<T>::getVector(){ return this->internalVector; }
+
+template<typename T>
+void selectionSort<T>::loadVector(std::vector<T> &orginVector){
+  for(T object:orginVector){
+    this->internalVector.push_back(object);
+  }
+}
+
+template<typename T>
+void selectionSort<T>::sort(){
+  int indexOfMinValue = 0;
+
+  for (unsigned int x = 0; x < this->internalVector.size()-1; x++){
+    indexOfMinValue = x;
+    for (unsigned int y = x+1; y < this->internalVector.size()-1; y++){
+      if (this->internalVector[y] < this->internalVector[x]){ indexOfMinValue = y; }
+
+      // Time to swap
+      T temp = this->internalVector[indexOfMinValue];
+      this->internalVector[indexOfMinValue] = this->internalVector[x];
+      this->internalVector[x] = temp;
+    }
+  }
+}
+
+#endif
